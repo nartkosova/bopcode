0d364bea0bacebb454054d667abfca699a7381fe
nartkosova <nartkosova21@gmail.com>
2023-09-05 14:51:27 +0300

    Ask the use to give 2 numbers

--- asignment/src/asignment/Hamk1.java
+++ asignment/src/asignment/Hamk1.java
@@ -1,9 +1,19 @@
 package asignment;
 
+import java.util.Scanner;
+
 public class Hamk1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner (System.in);
+		System.out.println("Please give me a number:");
+		String answer = in.nextLine();
+		int answer1 = Integer.parseInt(answer);
+		
+		System.out.println("Please give me another number:");
+		String answr = in.nextLine();
+		int answer2 = Integer.parseInt(answr);
+		
 
 	}
 
